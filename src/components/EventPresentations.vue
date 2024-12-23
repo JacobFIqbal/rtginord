b85814e061b42267a5fdac8ea262436781b633bd
Jacob Forsdahl Iqbal <jacobiqbal2003@gmail.com>
2024-12-24 00:35:57 +0100

    another reasearcher and chages

--- src/components/EventPresentations.vue
+++ src/components/EventPresentations.vue
@@ -7,13 +7,20 @@
         <img src="@/assets/SigneHForsdahl.png" alt="Signe Helene Forsdahl" class="presenter-image" />
         <p class="presenter-name">Signe Helene Forsdahl</p>
         <p class="presenter-name">Radiolog, UNN</p>
+        <a href="https://www.researchgate.net/profile/Signe-Forsdahl" target="_blank" class="researchgate-link">
+          ResearchGate Profile
+        </a>
 
       </div>
       
       <!-- Placeholder Presenters -->
       <div class="presenter-card">
-        <img src="" alt="Who" class="presenter-image" />
-        <p class="presenter-name">Who</p>
+        <img src="@/assets/AmjidIqbal.jpg" alt="Amjid Iqbal" class="presenter-image" />
+        <p class="presenter-name">Amjid Iqbal</p>
+        <p class="presenter-name">Cardiologist, Rikshospitalet</p>
+        <a href="https://www.researchgate.net/profile/Amjid-Iqbal" target="_blank" class="researchgate-link">
+          ResearchGate Profile
+        </a>
       </div>
       <div class="presenter-card">
         <img src="" alt="Who 2" class="presenter-image" />
@@ -50,7 +57,9 @@
 }
 
 .presenter-image {
-  max-width: 100%;
+  /*max-width: 90%;*/
+  max-width: 200px;
+  max-height: 200px;
   height: auto;
   border-radius: 50%;
   margin-bottom: 10px;
@@ -60,5 +69,17 @@
   font-size: 1.2rem;
   font-weight: bold;
   color: #55ad9b;
+}
+.researchgate-link {
+  font-size: 0.9rem;
+  color: #55ad9b;
+  text-decoration: none;
+  font-weight: bold;
+  margin-top: 10px;
+  display: inline-block;
+}
+
+.researchgate-link:hover {
+  text-decoration: underline;
 }
 </style>
